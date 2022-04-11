4c3a503f460a1675017310147a6c2ddb0f8d27eb
ShinySp00n <96329459+ShinySp00n@users.noreply.github.com>
2022-04-11 21:39:17 +1000

    Delete password_generator.sh

--- password_generator.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-echo "Enter the amount of characters and passwords you want: "
-read PASS_LENGTH PASS_NUMB
-
-for p in $(seq 1 $PASS_NUMB); do
-	openssl rand -base64 48 | cut -c1-$PASS_LENGTH
-done
